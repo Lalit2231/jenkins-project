20142e884192eebe0528b0eae950582e81cd4b2a
Lalitnarayan <lalitd@cirruscloudsystems.com>
2025-03-28 15:55:46 +0530

    Student Object Added

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ class Demo{
     public static void main(String[] args) {
         System.out.println("This java file used for jenkin demo project");
         System.out.println("New Message added");
+
+        Student student = new Student();
     }
 }
